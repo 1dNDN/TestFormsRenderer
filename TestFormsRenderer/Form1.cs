5be79ba4edbb803dba56745fd0862aa7f20bfb31
1dNDN <idndn@inbox.ru>
2021-01-07 05:20:12 +0300

    Чета было сделано

--- TestFormsRenderer/Form1.cs
+++ TestFormsRenderer/Form1.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Drawing;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -8,171 +7,169 @@ using System.Windows.Forms;
 
 namespace TestFormsRenderer
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        public bool IsAnimationRunning;
-        public DirectBitmap bmp;
+		public bool IsAnimationRunning;
+		public DirectBitmap bmp;
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (!IsAnimationRunning) new Thread(Animate).Start();
-        }
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (!IsAnimationRunning) new Thread(Animate).Start();
+		}
 
-        public void Animate()
-        {
-            IsAnimationRunning = true;
-            Pixel[,] pixels = new Pixel[256, 256];
-            FillPixelArray(78, 178, 56, 156, ref pixels);
-            bmp = new DirectBitmap(pixels.GetLength(0), pixels.GetLength(1));
-            DisplayPixelArray(ref pixels, true);
-            Thread.Sleep(500);
-            bool moved = true;
+		public void Animate()
+		{
+			IsAnimationRunning = true;
+			Pixel[,] pixels = new Pixel[256, 256];
+			FillPixelArray(78, 178, 56, 156, ref pixels);
+			bmp = new DirectBitmap(pixels.GetLength(0), pixels.GetLength(1));
+			DisplayPixelArray(ref pixels, true);
+			Thread.Sleep(500);
+			bool moved = true;
 
-            long displayTicks = 0;
-            long tickTicks = 0;
-            while (moved)
-            {
-                moved = false;
-                Stopwatch tickStopwatch = new Stopwatch();
-                Stopwatch displayStopwatch = new Stopwatch();
-                tickStopwatch.Start();
-                TickSimulation(ref pixels, ref moved);
-                tickStopwatch.Stop();
-                displayStopwatch.Start();
-                DisplayPixelArray(ref pixels);
-                displayStopwatch.Stop();
-                displayTicks += displayStopwatch.ElapsedTicks;
-                tickTicks += tickStopwatch.ElapsedTicks;
-                Console.WriteLine(displayStopwatch.ElapsedTicks + " " + tickStopwatch.ElapsedTicks);
-            }
-            
-            Console.WriteLine(displayTicks);
-            Console.WriteLine(tickTicks);
+			long displayTicks = 0;
+			long tickTicks = 0;
+			while (moved)
+			{
+				moved = false;
+				Stopwatch tickStopwatch = new Stopwatch();
+				Stopwatch displayStopwatch = new Stopwatch();
+				tickStopwatch.Start();
+				TickSimulation(ref pixels, ref moved);
+				tickStopwatch.Stop();
+				displayStopwatch.Start();
+				DisplayPixelArray(ref pixels);
+				displayStopwatch.Stop();
+				displayTicks += displayStopwatch.ElapsedTicks;
+				tickTicks += tickStopwatch.ElapsedTicks;
+				Console.WriteLine(displayStopwatch.ElapsedTicks + " " + tickStopwatch.ElapsedTicks);
+			}
 
-            IsAnimationRunning = false;
-        }
-        
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe void TickSimulation(ref Pixel[,] pixels, ref bool moved)
-        {
-            int xLenght = pixels.GetLength(0);
-            int yLenght = pixels.GetLength(1);
-            int xLenghtMunus = yLenght - 1;
-            int yLanghtMunus = xLenght - 1;
-            for (int y = xLenghtMunus; y >= 0; y--)
-            for (int x = 0; x < xLenght; x++)
-            {
-                if (!pixels[x, y].Color)
-                    continue;
-                if (pixels[x, y].Ticked)
-                    continue;
-                if (x == 0)
-                    continue;
-                if (x == xLenghtMunus)
-                    continue;
-                if (y == yLanghtMunus)
-                    continue;
-                int yPlus = y + 1;
-                if (!pixels[x, yPlus].Color)
-                {
-                    pixels[x, y].Color = false;
-                    pixels[x, y].Ticked = true;
-                    pixels[x, yPlus].Color = true;
-                    pixels[x, yPlus].Ticked = true;
-                    moved = true;
-                    continue;
-                }
+			Console.WriteLine(displayTicks);
+			Console.WriteLine(tickTicks);
 
-                int xMunus = x - 1;
-                if (!pixels[xMunus, yPlus].Color)
-                {
-                    pixels[x, y].Color = false;
-                    pixels[x, y].Ticked = true;
-                    pixels[xMunus, yPlus].Color = true;
-                    pixels[xMunus, yPlus].Ticked = true;
-                    moved = true;
-                    continue;
-                }
+			IsAnimationRunning = false;
+		}
 
-                int xPlus = x + 1;
-                if (!pixels[xPlus, yPlus].Color)
-                {
-                    pixels[x, y].Color = false;
-                    pixels[x, y].Ticked = true;
-                    pixels[xPlus, yPlus].Color = true;
-                    pixels[xPlus, yPlus].Ticked = true;
-                    moved = true;
-                }
-            }
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void TickSimulation(ref Pixel[,] pixels, ref bool moved)
+		{
+			int xLenght = pixels.GetLength(0);
+			int yLenght = pixels.GetLength(1);
+			int xLenghtMunus = yLenght - 1;
+			int yLanghtMunus = xLenght - 1;
+			for (int y = xLenghtMunus; y >= 0; y--)
+			for (int x = 0; x < xLenght; x++)
+			{
+				if (!pixels[x, y].Color)
+					continue;
+				if (pixels[x, y].Ticked)
+					continue;
+				if (x == 0)
+					continue;
+				if (x == xLenghtMunus)
+					continue;
+				if (y == yLanghtMunus)
+					continue;
+				int yPlus = y + 1;
+				if (!pixels[x, yPlus].Color)
+				{
+					pixels[x, y].Color = false;
+					pixels[x, y].Ticked = true;
+					pixels[x, yPlus].Color = true;
+					pixels[x, yPlus].Ticked = true;
+					moved = true;
+					continue;
+				}
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe void FillPixelArray(int xMin, int xMax, int yMin, int yMax, ref Pixel[,] pixels)
-        {
-            for (int x = 0; x < pixels.GetLength(0); x++)
-            for (int y = 0; y < pixels.GetLength(1); y++)
-                if (x < xMax && x > xMin && y < yMax && y > yMin)
-                    pixels[x, y] = new Pixel(true);
-                else
-                    pixels[x, y] = new Pixel();
-        }
+				int xMunus = x - 1;
+				if (!pixels[xMunus, yPlus].Color)
+				{
+					pixels[x, y].Color = false;
+					pixels[x, y].Ticked = true;
+					pixels[xMunus, yPlus].Color = true;
+					pixels[xMunus, yPlus].Ticked = true;
+					moved = true;
+					continue;
+				}
 
-        DateTime lastUpdate = DateTime.Now;
+				int xPlus = x + 1;
+				if (!pixels[xPlus, yPlus].Color)
+				{
+					pixels[x, y].Color = false;
+					pixels[x, y].Ticked = true;
+					pixels[xPlus, yPlus].Color = true;
+					pixels[xPlus, yPlus].Ticked = true;
+					moved = true;
+				}
+			}
+		}
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private unsafe void DisplayPixelArray(ref Pixel[,] pixels, bool Cached = false)
-        {
-            for (int x = 0; x < pixels.GetLength(0); x++)
-            for (int y = 0; y < pixels.GetLength(1); y++)
-            {
-                if (pixels[x, y].Ticked)
-                {
-                    bmp.SetPixel(x, y, pixels[x, y].Color ? -16777216 : -1);
-                    pixels[x, y].Ticked = false;
-                }
-                else if (Cached)
-                {
-                    bmp.SetPixel(x, y, pixels[x, y].Color ? -16777216 : -1);
-                    pixels[x, y].Ticked = false;
-                }
-            }
-            
-            if((DateTime.Now - lastUpdate).Milliseconds > 30)
-            {
-                lastUpdate = DateTime.Now; 
-                pictureBox1.Image = bmp.Bitmap;
-            }
-        }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void FillPixelArray(int xMin, int xMax, int yMin, int yMax, ref Pixel[,] pixels)
+		{
+			for (int x = 0; x < pixels.GetLength(0); x++)
+			for (int y = 0; y < pixels.GetLength(1); y++)
+				if (x < xMax && x > xMin && y < yMax && y > yMin)
+					pixels[x, y] = new Pixel(true);
+				else
+					pixels[x, y] = new Pixel();
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            AllocConsole();
-        }
+		private DateTime lastUpdate = DateTime.Now;
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        private static extern bool AllocConsole();
-    }
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private void DisplayPixelArray(ref Pixel[,] pixels, bool Cached = false)
+		{
+			for (int x = 0; x < pixels.GetLength(0); x++)
+			for (int y = 0; y < pixels.GetLength(1); y++)
+				if (pixels[x, y].Ticked)
+				{
+					bmp.SetPixel(x, y, pixels[x, y].Color ? -16777216 : -1);
+					pixels[x, y].Ticked = false;
+				}
+				else if (Cached)
+				{
+					bmp.SetPixel(x, y, pixels[x, y].Color ? -16777216 : -1);
+					pixels[x, y].Ticked = false;
+				}
 
-    public class Pixel
-    {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public Pixel(bool Color = false, bool Ticked = false, bool Static = false)
-        {
-            this.Color = Color;
-            this.Ticked = Ticked;
-            this.Static = Static;
-        }
+			if ((DateTime.Now - lastUpdate).Milliseconds > 30)
+			{
+				lastUpdate = DateTime.Now;
+				pictureBox1.Image = bmp.Bitmap;
+			}
+		}
 
-        public bool Color;
+		[DllImport("kernel32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool AllocConsole();
 
-        public bool Ticked;
+		private void Form1_Load_1(object sender, EventArgs e)
+		{
+			AllocConsole();
+		}
+	}
 
-        public bool Static;
-    }
+	public class Pixel
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public Pixel(bool Color = false, bool Ticked = false, bool Static = false)
+		{
+			this.Color = Color;
+			this.Ticked = Ticked;
+			this.Static = Static;
+		}
+
+		public bool Color;
+
+		public bool Ticked;
+
+		public bool Static;
+	}
 }
